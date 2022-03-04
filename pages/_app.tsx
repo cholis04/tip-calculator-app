f22b8e29cd8b4ba60b74f98c466c505124b2ba4d
cholis04 <korunkolis04@gmail.com>
2022-03-04 10:19:44 +0700

    Move GlobalStyle into the App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,33 @@
 import type { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
 
 // Font Source
 import '@fontsource/space-mono';
 
-import { GlobalStyle } from '../styles/globalStyle';
+// Global Style
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-family: "Space Mono";
+    background-color:hsl(185, 41%, 84%);
+  }
+  img {
+    display: block;
+    max-width: 100%;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
